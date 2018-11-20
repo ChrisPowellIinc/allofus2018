aabdf52cb1ea9bd8fac0d0d4df8cffb69c82d3c4
Spankie Dee <odohi.david@gmail.com>
2018-11-20 09:16:15 +0100

    add login component

---
+++ www/js/src/components/login.js
@@ -0,0 +1,25 @@
+import m from "mithril";
+import ons from "onsenui";
+
+var Login = {
+    view: vnode => (
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 mx-auto">
+                    <h1> Login here </h1>
+                    <div>
+                        <input class="text-input text-input--material w-100" placeholder="Username" type="text" required />
+                    </div>
+                    <br/>
+                    <div>
+                        <input class="text-input text-input--material w-100" placeholder="Password" type="password" required/>
+                    </div>
+                    <ons-button class="button button--material w-100" onclick={() => {
+                        ons.notification.alert('Hello people!!');
+                    }}> Login </ons-button>
+                </div>
+            </div>
+        </div>)
+}
+
+export default Login;
