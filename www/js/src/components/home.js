192ba520ca5f9036d7404430c11cbf7c20ad099b
Spankie Dee <odohi.david@gmail.com>
2018-11-20 09:49:52 +0100

    linting.

--- www/js/src/components/home.js
+++ www/js/src/components/home.js
@@ -1,11 +1,11 @@
 import m from "mithril";
 
 var Home = {
-    view: vnode => (
-        <div>
-            <h1>Welcome to All of Us</h1>
-        </div>
-    )
-}
+  view: vnode => (
+    <div>
+      <h1>Welcome to All of Us</h1>
+    </div>
+  )
+};
 
 export default Home;
