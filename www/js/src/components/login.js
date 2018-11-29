10a29ed624191936f145cc85c7062713035165ef
Spankie Dee <odohi.david@gmail.com>
2018-11-29 11:55:26 +0100

    modify login page for validation

--- www/js/src/components/login.js
+++ www/js/src/components/login.js
@@ -1,12 +1,12 @@
 import m from "mithril";
 // import ons from "onsenui";
-import UserModel from "models/user";
+import Auth from "services/auth";
 
 var Login = {
-  Validate: () => {
-    if (!UserModel.User.username || !UserModel.User.password) {
-      return vnode.state.errors.push("Please fill out all forms");
-    }
+  login: e => {
+    e.preventDefault();
+    console.log("hello login");
+    Auth.login(Auth.user).catch(err => {});
   },
   oncreate: vnode => {
     vnode.state.errors = [];
@@ -14,61 +14,68 @@ var Login = {
   view: vnode => (
     <div class="container">
       <div class="row">
-        <div class="col-md-6 mx-auto my-5 text-center">
-          <h1> Login </h1>
-          <div class="my-4">
-            <input
-              class="text-input text-input--material w-100"
-              placeholder="Username"
-              type="text"
-              value={UserModel.User.username}
-              oninput={m.withAttr("value", value => {
-                UserModel.User.username = value.trim();
-              })}
-              required
-            />
-          </div>
+        <form
+          class="w-100"
+          onsubmit={Login.login}
+          autocomplete="off"
+          novalidate
+        >
+          <div class="col-md-6 mx-auto my-5 text-center">
+            <h1> Login </h1>
+            <div class="my-4">
+              <input
+                class="text-input text-input--material w-100"
+                placeholder="Username"
+                type="text"
+                value={Auth.user.username}
+                oninput={m.withAttr("value", value => {
+                  Auth.setUsername(value);
+                })}
+                required
+              />
+              {Auth.errors.username && (
+                <small class="form-text text-danger">
+                  {Auth.errors.username}
+                </small>
+              )}
+            </div>
 
-          <div class="my-4">
-            <input
-              class="text-input text-input--material w-100"
-              placeholder="Password"
-              type="password"
-              value={UserModel.User.password}
-              oninput={m.withAttr("value", value => {
-                UserModel.User.password = value;
-              })}
-              required
-            />
+            <div class="my-4">
+              <input
+                class="text-input text-input--material w-100"
+                placeholder="Password"
+                type="password"
+                value={Auth.user.password}
+                oninput={m.withAttr("value", value => {
+                  Auth.setPassword(value);
+                })}
+                required
+              />
+              {Auth.errors.password && (
+                <small class="form-text text-danger">
+                  {Auth.errors.password}
+                </small>
+              )}
+            </div>
+            {vnode.state.loading && (
+              <ons-progress-circular
+                class="white-text"
+                style="color: white"
+                indeterminate
+              />
+            )}
+            <button
+              class="button button--material w-100 mt-3"
+              type="submit"
+              disabled={vnode.state.loading}
+              onclick={() => {
+                console.log("errors");
+              }}
+            >
+              Login
+            </button>
           </div>
-          {vnode.state.loading && (
-            <ons-progress-circular
-              class="white-text"
-              style="color: white"
-              indeterminate
-            />
-          )}
-          <ons-button
-            class="button button--material w-100 mt-3"
-            onclick={() => {
-              vnode.state.errors = Login.Validate();
-              vnode.state.loading = true;
-              // ons.notification.alert("Hello people!!");
-              UserModel.Login()
-                .then(resp => {
-                  console.log(resp);
-                })
-                .catch(err => {
-                  console.log(err);
-                })
-                .finally(() => {
-                  vnode.state.loading = false;
-                });
-            }}
-          >
-            Login
-          </ons-button>
-        </div>
+        </form>
       </div>
     </div>
   )
