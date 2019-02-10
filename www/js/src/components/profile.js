06c626f09b662dff82e2f7efbc936aeb62c08eca
Spankie Dee <odohi.david@gmail.com>
2019-02-10 16:27:34 +0100

    refactor the profile page to allow for choosing profile pic and uploading it

---
+++ www/js/src/components/profile.js
@@ -0,0 +1,48 @@
+import m from "mithril";
+import ProfileService from "services/profile.js";
+
+var Profile = {
+  UploadImage: e => {
+    
+    if (e.target.files[0]) {
+      ProfileService.UploadImage(e.target.files[0]);
+    }
+  },
+  view: vnode => (
+    <section>
+      <div class="p-3">
+        <div class="b-2 text-center">
+          <img
+            src="img/logo.png"
+            class="img image-responsive"
+            alt="Profile"
+            width="120"
+            height="120"
+          />
+        </div>
+        <div class="text-center">
+          <input class="d-none" id="uploadimage" type="file" name="profile_image" onchange={e => {
+            Profile.UploadImage(e);
+          }} />
+          <label for="uploadimage" class="btn btn-sm btn-primary">
+            <i class="fa fa-trash" />
+            Change image
+          </label>
+        </div>
+        My profile page...
+      </div>
+      <div class="bottom-bar">
+        <div class="bottom-bar__line-height" style="text-align:center">
+          <span class="toolbar-button text-info">
+            <button class="btn btn-sm">
+              <i class="fa fa-trash" />
+              Settings
+            </button>
+          </span>
+        </div>
+      </div>
+    </section>
+  )
+};
+
+export default Profile;
