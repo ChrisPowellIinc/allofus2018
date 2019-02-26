66eecd8e9f772b01bcfb6d84972040cfe1feb244
Spankie Dee <odohi.david@gmail.com>
2019-02-26 22:34:19 +0100

    create messages component

---
+++ www/js/src/components/messages.js
@@ -0,0 +1,14 @@
+import m from "mithril";
+
+import Footer from "components/homefooter";
+
+var Messages = {
+  view: vnode => (
+    <section>
+      Messages feeds goes here...
+      <Footer />
+    </section>
+  )
+};
+
+export default Messages;
