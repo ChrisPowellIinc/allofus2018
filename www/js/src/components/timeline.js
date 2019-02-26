5c87dcef880620ae0bd5afbee1b4fad0511199b0
Spankie Dee <odohi.david@gmail.com>
2019-02-26 22:34:53 +0100

    create timeline component

---
+++ www/js/src/components/timeline.js
@@ -0,0 +1,14 @@
+import m from "mithril";
+
+import Footer from "components/homefooter";
+
+var TimeLine = {
+  view: vnode => (
+    <section>
+      Time Line feeds goes here...
+      <Footer />
+    </section>
+  )
+};
+
+export default TimeLine;
