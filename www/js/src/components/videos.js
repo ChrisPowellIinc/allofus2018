d37083e9621c64fb474d9f31fa43d60f7736d556
Spankie Dee <odohi.david@gmail.com>
2019-02-26 22:35:01 +0100

    create videos component

---
+++ www/js/src/components/videos.js
@@ -0,0 +1,14 @@
+import m from "mithril";
+
+import Footer from "components/homefooter";
+
+var Videos = {
+  view: vnode => (
+    <section>
+      Videos feeds goes here...
+      <Footer />
+    </section>
+  )
+};
+
+export default Videos;
