bdf9c8b86c51b977b650ac6a6febfda2e420bbe2
Spankie Dee <odohi.david@gmail.com>
2019-04-28 21:52:52 +0100

    make the video component access the camera and display the video on the screen...

--- www/js/src/components/videos.js
+++ www/js/src/components/videos.js
@@ -1,13 +1,27 @@
 import m from "mithril";
-
-import Footer from "components/homefooter";
+import Shell from "containers/shell";
+import RTCservice from "services/rtc";
 
 var Videos = {
+  oncreate: () => {
+    console.log("Create the user media here");
+    const localVideo = document.getElementById("myvideo");
+    RTCservice.localVideo = localVideo;
+    RTCservice.GetUserMedia();
+  },
+  onremove: () => {
+    console.log("remove this vnode");
+    RTCservice.stop();
+  },
   view: vnode => (
-    <section>
-      Videos feeds goes here...
-      <Footer />
-    </section>
+    <Shell>
+      <section class="h-100">
+        <div class="" style="height: calc(100% - 44px)">
+          <video id="myvideo" class="h-100" autoplay muted playsinline />
+          <video id="remotevideo" class="h-100" autoplay muted playsinline />
+        </div>
+      </section>
+    </Shell>
   )
 };
 
