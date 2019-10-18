a6e26d30c00fd732fa04380194d44478bad058ad
Odohi David <odohi.david@gmail.com>
2019-10-19 00:02:31 +0100

    update the look and feel of videos

--- www/js/src/components/videos.js
+++ www/js/src/components/videos.js
@@ -6,32 +6,39 @@ import RTCService from "services/rtc";
 
 const Call = {
   oncreate: vnode => {
-    if (vnode.attrs.type === "call") {
+    if (vnode.attrs.status === "call") {
       RTCService.call(vnode.attrs.user.email).then(res => {
         // RTCService.call();
       });
     }
   },
   view: vnode => {
-    const { hangup } = vnode.attrs;
+    const { hangup, status, user } = vnode.attrs;
     return (
-      <section class="container">
-        <div class="">
-          <p>Calling: {vnode.attrs.user.email || "no user yet"}</p>
-        </div>
-        <video
-          id="localVideo"
-          width="100%"
-          height="40%"
-          autoplay
-          playsinline
-          muted
+      <section class="">
+        {status === "call" ? (
+          <div class="callingmessage">
+            <p>calling: {user.email || "no user yet"}</p>
+          </div>
+        ) : (
+          ""
+        )}
+        <div
+          id="localVideoContainer"
+          class={status === "call" ? "video-full" : "video-small"}
         >
-          <track src="" kind="captions" srclang="en" label="English" />
-        </video>
-        <video id="remoteVideo" width="100%" height="40%" autoplay playsinline>
-          <track src="" kind="captions" srclang="en" label="English" />
-        </video>
+          <video id="localVideo" autoplay playsinline muted>
+            <track src="" kind="captions" srclang="en" label="English" />
+          </video>
+        </div>
+        <div
+          id="remoteVideoContainer"
+          class={status === "call" ? "video-small" : "video-full"}
+        >
+          <video id="remoteVideo" autoplay playsinline>
+            <track src="" kind="captions" srclang="en" label="English" />
+          </video>
+        </div>
         <ons-fab onclick={hangup} position="bottom right" class="bg-danger">
           <ons-icon icon="md-close" style="color: white;" />
         </ons-fab>
@@ -44,6 +51,18 @@ var Videos = {
   oncreate: () => {
     Videos.getUsers();
     RTCService.HandleReply();
+    // this means the other person has accepted the call.
+    document.addEventListener(
+      "accept",
+      data => {
+        // you made the call that is why you are receiving this event.
+        // so the localstream has been created already.
+        RTCService.MakePeer();
+        Videos.state.page = "accept";
+        m.redraw();
+      },
+      false
+    );
     // Run function on `declinecall` event
     document.addEventListener("call", data => {
       const message = data.detail;
@@ -109,32 +128,37 @@ var Videos = {
     <section>
       {Videos.state.page === "list" ? (
         <section>
-          <ons-list-header>Users</ons-list-header>
-          {Videos.state.users.length
-            ? Videos.state.users.map(user => (
-                <ons-list-item
-                  onclick={() => {
-                    Videos.call(user);
-                  }}
-                >
-                  <div class="left">
-                    <img
-                      class="list-item__thumbnail"
-                      src="https://placekitten.com/g/40/40"
-                    />
-                  </div>
-                  <div class="center">
-                    <span class="list-item__title">{`${user.first_name} ${user.last_name}`}</span>
-                    <span class="list-item__subtitle">On the Internet</span>
-                  </div>
-                </ons-list-item>
-              ))
-            : ""}
+          <ons-list
+            style="width: 100%"
+            class="list list--material overflow-auto content-height"
+          >
+            <ons-list-header>Users</ons-list-header>
+            {Videos.state.users.length
+              ? Videos.state.users.map(user => (
+                  <ons-list-item
+                    onclick={() => {
+                      Videos.call(user);
+                    }}
+                  >
+                    <div class="left">
+                      <img
+                        class="list-item__thumbnail"
+                        src="https://placekitten.com/g/40/40"
+                      />
+                    </div>
+                    <div class="center">
+                      <span class="list-item__title">{`${user.first_name} ${user.last_name}`}</span>
+                      <span class="list-item__subtitle">On the Internet</span>
+                    </div>
+                  </ons-list-item>
+                ))
+              : ""}
+          </ons-list>
         </section>
       ) : Videos.state.page === "call" || Videos.state.page === "accept" ? (
         <Call
           user={Videos.state.user}
-          type={Videos.state.page}
+          status={Videos.state.page}
           hangup={Videos.hangup}
         />
       ) : (
